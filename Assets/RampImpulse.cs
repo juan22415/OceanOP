a1d842bf6bd9213cef7f9af0d131a84f814091ad
juan camilo rios <juan22415@hotmail.com>
2017-09-10 15:12:47 -0500

    Escena multiplayer y una rampa

---
+++ Assets/RampImpulse.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RampImpulse : MonoBehaviour {
+
+
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+
+            collision.gameObject.GetComponent<Rigidbody>().AddForce(collision.gameObject.transform.forward * 3,ForceMode.Impulse);
+
+    }
+}
