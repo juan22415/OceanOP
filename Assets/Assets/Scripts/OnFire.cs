5c52dc443518661bdeca1e76294d96f2fea6e86d
Manuela Recio <manu138@hotmail.es>
2017-09-20 23:09:03 -0500

    Listo el fuego y los estados creo.. pero no se porque no tengo dos controles para probar, también puse el sonido de quemarse en la carpeta de sounds.

---
+++ Assets/Assets/Scripts/OnFire.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnFire : MonoBehaviour {
+    public GameObject firstState;
+    public GameObject secondState;
+    public GameObject finalState;
+    public int counterState;
+    private bool isColliding;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        isColliding = false;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (isColliding) return;
+        {
+            isColliding = true;
+        
+                if (collision.gameObject.CompareTag("bullet"))
+                {
+                    counterState++;
+                    if(counterState==1)
+                    {
+                        firstState.SetActive(true);
+                    }
+                    if (counterState == 2)
+                    {
+                        secondState.SetActive(true);
+                    }
+                    if (counterState == 3)
+                    {
+                        finalState.SetActive(true);
+                    }
+                }
+        }
+    }
+}
