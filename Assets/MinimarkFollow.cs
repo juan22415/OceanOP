b748bc49ef554c02d12bffabddb3fbb8d13b54bf
juan camilo rios <juan22415@hotmail.com>
2017-09-30 17:52:54 -0500

    minimapa ya no rota

---
+++ Assets/MinimarkFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimarkFollow : MonoBehaviour {
+
+    [SerializeField] private GameObject Playertofollow;
+
+
+	void Update () {
+
+        gameObject.transform.position = new Vector3(Playertofollow.transform.position.x,
+                                                    300,
+                                                    Playertofollow.transform.position.z);
+
+        Quaternion rotation=  new Quaternion(   0,
+                                                Playertofollow.transform.localRotation.y,
+                                                0,
+                                                Playertofollow.transform.localRotation.w);
+
+        gameObject.transform.localRotation = rotation;
+	}
+}
