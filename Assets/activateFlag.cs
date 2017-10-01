08a55b470b73ce8ad8fc58f7cf498ac8d961a62b
juan camilo rios <juan22415@hotmail.com>
2017-09-30 20:20:06 -0500

    banderas

---
+++ Assets/activateFlag.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class activateFlag : MonoBehaviour {
+
+    [SerializeField] private GameObject[] flags;
+    public int activeFlag;
+
+
+    private void Start()
+    {
+        activeFlag = Random.Range(0, 5);
+        flags[activeFlag].SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
