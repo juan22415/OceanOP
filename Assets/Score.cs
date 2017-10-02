f61623914e79c749d045eec36cd03b74dc6d3ba6
juan camilo rios <juan22415@hotmail.com>
2017-10-02 18:55:55 -0500

    Score

---
+++ Assets/Score.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+    public Text scoreText;
+    public int score;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Flag"))
+        {
+            score++;
+            scoreText.text = score.ToString();
+        }
+    }
+}
