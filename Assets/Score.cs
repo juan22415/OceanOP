29634b6cb82397e51b8418f318feb7a272944651
juan camilo rios <juan22415@hotmail.com>
2017-10-08 23:02:53 -0500

    Winner

--- Assets/Score.cs
+++ Assets/Score.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Score : MonoBehaviour {
 
     public Text scoreText;
+    public Text winnerText;
+
     public int score;
 
 
@@ -15,6 +18,24 @@ public class Score : MonoBehaviour {
         {
             score++;
             scoreText.text = score.ToString();
+            CheckWinner(score);
         }
+    }
+
+    public void CheckWinner(int currentFlags)
+    {
+        if (currentFlags == 3)
+        {
+            winnerText.gameObject.SetActive(true);
+            Debug.Log("winer");
+            StartCoroutine(Timer());
+
+        }
+    }
+
+    IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(5);
+        SceneManager.LoadScene(0);
     }
 }
