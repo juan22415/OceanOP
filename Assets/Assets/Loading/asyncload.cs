3183cb09707cd911bd6332994b97c0a3ec5cdf6f
juan camilo rios <juan22415@hotmail.com>
2017-11-12 14:28:09 -0500

    pantallas de carga y controles

---
+++ Assets/Assets/Loading/asyncload.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class asyncload : MonoBehaviour {
+
+    public int sceneToLoad;
+    public float waitTime;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(LoadNewScene());
+    }
+
+    
+
+    IEnumerator LoadNewScene()
+    {
+
+        // This line waits for 3 seconds before executing the next line in the coroutine.
+        // This line is only necessary for this demo. The scenes are so simple that they load too fast to read the "Loading..." text.
+        yield return new WaitForSeconds(waitTime);
+
+        // Start an asynchronous operation to load the scene that was passed to the LoadNewScene coroutine.
+        AsyncOperation async = SceneManager.LoadSceneAsync(sceneToLoad);
+
+        // While the asynchronous operation to load the new scene is not yet complete, continue waiting until it's done.
+        while (!async.isDone)
+        {
+            yield return null;
+        }
+
+    }
+}
--- Assets/Menu.cs
+++ Assets/Menu.cs
@@ -7,6 +7,6 @@ public class Menu : MonoBehaviour {
 
    public void onClick()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(2);
     }
 }
